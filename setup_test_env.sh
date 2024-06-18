cdd0d4d9a5c2013403dd30bd941d2048b66ba4a1
Mark Burggraf <markb@mantisbible.com>
2024-06-18 07:31:33 -0700

    fixed setup script for linux

--- setup_test_env.sh
+++ setup_test_env.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Install Rust Toolchain
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 
 # Install pgrx dependencies
-brew install git icu4c pkg-config
+# brew install git icu4c pkg-config
+sudo apt-get install git icu4c pkg-config
 
 # Install pgrx
 cargo install cargo-pgrx --version 0.11.3 --locked
